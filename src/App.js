b130f0131976ba3989a39e4c204450791285fa9e
justcode365 <justcode365@163.com>
2018-02-08 17:22:44 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react'
+import logo from './logo.svg'
+import './App.css'
+// import { moduleA } from './components/Button'
 
 class App extends Component {
+  state = { text: '', tags: [] }
+
+  async componentDidMount() {
+    const res = await fetch('https://conduit.productionready.io/api/tags')
+    const { tags } = await res.json()
+    this.setState({ tags })
+  }
+
+  handleClick = async () => {
+    const { moduleA } = await import('./components/Button')
+    console.warn(moduleA)
+    this.setState({ text: moduleA })
+  }
   render() {
+    console.warn(this.state)
+    const { tags } = this.state
     return (
-      <div className="App">
+      <Fragment>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h2>React Blog</h2>
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>Sign in</li>
+              <li>Sign up</li>
+            </ul>
+          </nav>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+        <section className="App-banner">
+          <h1>React Blog</h1>
+          <h3>A place to share your knowledge.</h3>
+        </section>
+
+        
+        <div className="App-blog">
+          <main>Global Feed</main>
+          <aside >
+            <p>Popular Tags</p>
+            {tags.map(tag => <a key={tag}>{tag}</a>)}
+            
+            <hr/>
+            <button onClick={this.handleClick}>
+              Load
+              {/* {moduleA} */}
+            </button>
+            Text : {this.state.text}
+          </aside>
+        </div>
+      </Fragment>
+    )
   }
 }
 
-export default App;
+export default App
