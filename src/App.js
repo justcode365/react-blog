70cdf81a2e00f8e1787c4dc696c9549a864f1c06
justcode365 <justcode365@163.com>
2018-02-09 15:14:24 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,19 +2,37 @@ import React, { Component, Fragment } from 'react'
 import Article from './components/Article'
 import './App.css'
 
+const API = 'https://conduit.productionready.io/api'
+
 class App extends Component {
-  state = { text: '', tags: [], articles: [], articlesCount: 0 }
+  state = { text: '', tags: [], articles: [], articlesCount: 0, page_no: 0 }
 
   async componentDidMount() {
     // 并行请求
     const [tagsResponse, articlesPromise] = await Promise.all([
-      fetch('https://conduit.productionready.io/api/tags'),
-      fetch('https://conduit.productionready.io/api/articles?limit=10&offset=0')
+      fetch(`${API}/tags`),
+      fetch(`${API}/articles?limit=10&offset=0`)
     ])
-    
+
     const { tags } = await tagsResponse.json()
     const { articles, articlesCount } = await articlesPromise.json()
     this.setState({ tags, articles, articlesCount })
+  }
+
+  setPage = async index => {
+    const { page_no } = this.state
+    const res = await fetch(
+      `${API}/articles?limit=10&offset=${page_no + index * 10}`
+    )
+
+    const { articles, articlesCount } = await res.json()
+    this.setState({ articles, articlesCount, page_no: page_no + index })
+  }
+
+  getTagArticles = async tag => {
+    const res = await fetch(`${API}/articles?tag=${tag}&limit=10&offset=0`)
+    const { articles, articlesCount } = await res.json()
+    this.setState({ articles, articlesCount, page_no: 0 })
   }
 
   handleClick = async () => {
@@ -23,7 +41,7 @@ class App extends Component {
   }
   render() {
     console.log(this.state)
-    const { tags, articles, articlesCount } = this.state
+    const { tags, articles, articlesCount, page_no } = this.state
     return (
       <Fragment>
         <header className="App-header container">
@@ -43,18 +61,47 @@ class App extends Component {
 
         <div className="App-blog container">
           <main>
-            <h3>Global Feed</h3>
+            <h3>
+              <a href="#">Global Feed</a>
+            </h3>
             {articles.map((post, i) => <Article key={i} post={post} />)}
             <p>
-              <a href=""> {'<'} </a>
-              1
-              <a href=""> {'>'} </a>
-               共 {articlesCount}
+              <a
+                href=""
+                onClick={e => {
+                  e.preventDefault()
+                  this.setPage(-1)
+                }}
+              >
+                {'<'}
+              </a>
+              {page_no + 1}
+              <a
+                href=""
+                onClick={e => {
+                  e.preventDefault()
+                  this.setPage(+1)
+                }}
+              >
+                {'>'}
+              </a>
+              共 {articlesCount}
             </p>
           </main>
           <aside>
             <p>Popular Tags</p>
-            {tags.map(tag => <a key={tag}>{tag}</a>)}
+            {tags.map(tag => (
+              <a
+                href=""
+                key={tag}
+                onClick={e => {
+                  e.preventDefault()
+                  this.getTagArticles(tag)
+                }}
+              >
+                {tag}
+              </a>
+            ))}
             <hr />
             <button onClick={this.handleClick}>dynamic import</button>
             Text : {this.state.text}
