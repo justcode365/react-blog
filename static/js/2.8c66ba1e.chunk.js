11dd6d803b7a25796845100315f17920d4c3cd4e
justcode365 <justcode365@163.com>
2018-02-10 00:02:58 +0800

    Updates

--- static/js/2.8c66ba1e.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([2],{89:function(e,n,o){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),o.d(n,"moduleA",function(){return t});var t="Hello"}});
-//# sourceMappingURL=2.8c66ba1e.chunk.js.map
