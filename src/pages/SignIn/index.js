b2142a6562d52d3481f7d3b0a77838ff3db6e90a
justcode365 <justcode365@163.com>
2018-02-11 23:40:38 +0800

    update

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 import './style.css'
 
 class SignIn extends Component {
+  state = { email: '', password: '', redirectToHome: false }
+  handleSubmit = async e => {
+    e.preventDefault()
+    const { email, password } = this.state
+
+    const url = 'https://conduit.productionready.io/api/users/login'
+    const options = {
+      method: 'post',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ user: { email, password } })
+    }
+
+    const res = await fetch(url, options)
+    const data = await res.json()
+    this.setState({ email: '', password: '', redirectToHome: true })
+  }
+
+  handleChange = e => {
+    const { name, value } = e.target
+    this.setState({ [name]: value })
+  }
+
   render() {
+    const { email, password, redirectToHome } = this.state
+
+    if (redirectToHome) return <Redirect to="/" />
     return (
-      <form action="" className="blog-form">
+      <form onSubmit={this.handleSubmit} className="blog-form">
         <h1>Sign In</h1>
         <Link to="register">Need an account?</Link>
         <p>
-          <input type="text" placeholder="Email" />
+          <input
+            type="text"
+            value={email}
+            placeholder="Email"
+            onChange={this.handleChange}
+            name="email"
+          />
         </p>
         <p>
-          <input type="text" placeholder="Password" />
+          <input
+            type="text"
+            value={password}
+            placeholder="Password"
+            onChange={this.handleChange}
+            name="password"
+          />
         </p>
 
         <p>
