17578144cdb2d92a717c6498ccc19d316bab3e57
justcode365 <justcode365@163.com>
2018-02-13 22:18:42 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React, { Fragment } from 'react'
-import { HashRouter, BrowserRouter, Route, Link } from 'react-router-dom'
-import asyncload from 'utils/asyncload'
-import 'App.css'
+import React, { Fragment } from "react";
+import { HashRouter, BrowserRouter, Route, Link } from "react-router-dom";
+import asyncload from "utils/asyncload";
+import styled from "styled-components";
 
-const Router = process.env.NODE_ENV === 'development' ? BrowserRouter : HashRouter
+const Router =
+  process.env.NODE_ENV === "development" ? BrowserRouter : HashRouter;
+
+const Header = styled.header`
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    height: 30px;
+    max-width: 1000px;
+  }
+
+   li {
+    display: inline-block;
+    margin-right: 10px;
+    color: #999;
+  }
+
+   h2 {
+    flex: 1;
+  }
+`;
 
 export default () => (
   <Router>
     <Fragment>
-      <header className="App-header container">
+      <Header>
         <h2>React Blog</h2>
         <nav>
           <ul>
@@ -23,13 +43,29 @@ export default () => (
             </li>
           </ul>
         </nav>
-      </header>
+      </Header>
 
-      <Route path="/" exact component={asyncload(() => import('containers/Home'))} />
-      <Route path="/login" component={asyncload(() => import('containers/SignIn'))} />
-      <Route path="/register" component={asyncload(() => import('containers/SignUp'))} />
-      <Route path="/@:username" component={asyncload(() => import('containers/Profile'))} />
-      <Route path="/article/:title" component={asyncload(() => import('containers/Article'))} />
+      <Route
+        path="/"
+        exact
+        component={asyncload(() => import("containers/Home"))}
+      />
+      <Route
+        path="/login"
+        component={asyncload(() => import("containers/SignIn"))}
+      />
+      <Route
+        path="/register"
+        component={asyncload(() => import("containers/SignUp"))}
+      />
+      <Route
+        path="/@:username"
+        component={asyncload(() => import("containers/Profile"))}
+      />
+      <Route
+        path="/article/:title"
+        component={asyncload(() => import("containers/Article"))}
+      />
     </Fragment>
   </Router>
-)
+);
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,49 +1,102 @@
-import React, { Component, Fragment } from 'react'
-import Item from 'components/Item'
-import { connect } from 'react-redux'
+import React, { Component, Fragment } from "react";
+import Item from "components/Item";
+import { connect } from "react-redux";
+import styled from "styled-components";
 
+const Banner = styled.section`
+  padding: 15px 0;
+  background-color: #888;
+  text-align: center;
+  color: #fff;
+
+  h1 {
+    text-shadow: 0px 1px 3px rgba(0, 0, 0, 0.3);
+    font-size: 40px;
+  }
+
+  h4 {
+    font-weight: normal;
+  }
+`;
+
+const Blog = styled.div`
+  display: flex;
+  margin-top: 30px;
+  max-width: 1000px;
+  align-items: flex-start;
+
+  main {
+    width: 75%;
+    margin-right: 30px;
+  }
+
+  aside {
+    padding: 10px;
+    width: 25%;
+    background-color: #f3f3f3;
+    border-radius: 5px;
+  }
+
+  aside p {
+    margin-top: 5px;
+    margin-bottom: 5px;
+  }
+
+  aside a {
+    background-color: #818a91;
+    margin-right: 10px;
+    display: inline-block;
+    border-radius: 15px;
+    color: #fff;
+    margin-top: 5px;
+    padding: 5px 8px;
+  }
+`;
 class Home extends Component {
-  state = { tags: [], articles: [], articlesCount: 0, page_no: 0 }
+  state = { tags: [], articles: [], articlesCount: 0, page_no: 0 };
 
   async componentDidMount() {
     // 并行请求
     const [tagsResponse, articlesPromise] = await Promise.all([
       fetch(`${process.env.REACT_APP_API}/tags`),
       fetch(`${process.env.REACT_APP_API}/articles?limit=5&offset=0`)
-    ])
+    ]);
 
-    const { tags } = await tagsResponse.json()
-    const { articles, articlesCount } = await articlesPromise.json()
-    this.setState({ tags, articles, articlesCount })
+    const { tags } = await tagsResponse.json();
+    const { articles, articlesCount } = await articlesPromise.json();
+    this.setState({ tags, articles, articlesCount });
   }
 
   setPage = async index => {
-    const { page_no } = this.state
+    const { page_no } = this.state;
     const res = await fetch(
-      `${process.env.REACT_APP_API}/articles?limit=5&offset=${page_no + index * 10}`
-    )
+      `${process.env.REACT_APP_API}/articles?limit=5&offset=${page_no +
+        index * 10}`
+    );
 
-    const { articles, articlesCount } = await res.json()
-    this.setState({ articles, articlesCount, page_no: page_no + index })
-  }
+    const { articles, articlesCount } = await res.json();
+    this.setState({ articles, articlesCount, page_no: page_no + index });
+  };
 
   getTagArticles = async tag => {
-    const res = await fetch(`${process.env.REACT_APP_API}/articles?tag=${tag}&limit=5&offset=0`)
-    const { articles, articlesCount } = await res.json()
-    this.setState({ articles, articlesCount, page_no: 0 })
-  }
+    const res = await fetch(
+      `${process.env.REACT_APP_API}/articles?tag=${tag}&limit=5&offset=0`
+    );
+    const { articles, articlesCount } = await res.json();
+    this.setState({ articles, articlesCount, page_no: 0 });
+  };
 
   render() {
-    const { tags, articles, articlesCount, page_no } = this.state
+    const { tags, articles, articlesCount, page_no } = this.state;
 
     return (
       <Fragment>
-        <section className="App-banner">
+        <Banner>
           <h1>React Blog</h1>
           <h3>A place to share your knowledge.</h3>
-        </section>
+        </Banner>
 
-        <div className="App-blog container">
+        <Blog>
           <main>
             <h3>
               <a href="#">Global Feed</a>
@@ -53,21 +106,21 @@ class Home extends Component {
               <a
                 href=""
                 onClick={e => {
-                  e.preventDefault()
-                  this.setPage(-1)
+                  e.preventDefault();
+                  this.setPage(-1);
                 }}
               >
-                {'<'}
+                {"<"}
               </a>
               {page_no + 1}
               <a
                 href=""
                 onClick={e => {
-                  e.preventDefault()
-                  this.setPage(+1)
+                  e.preventDefault();
+                  this.setPage(+1);
                 }}
               >
-                {'>'}
+                {">"}
               </a>
               共 {articlesCount}
             </p>
@@ -79,18 +132,18 @@ class Home extends Component {
                 href=""
                 key={tag}
                 onClick={e => {
-                  e.preventDefault()
-                  this.getTagArticles(tag)
+                  e.preventDefault();
+                  this.getTagArticles(tag);
                 }}
               >
                 {tag}
               </a>
             ))}
           </aside>
-        </div>
+        </Blog>
       </Fragment>
-    )
+    );
   }
 }
 
-export default connect(state => state)(Home)
+export default connect(state => state)(Home);
